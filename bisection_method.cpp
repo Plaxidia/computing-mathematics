b3f7abbe0ac01119e634ddf359dc9a0c3d4181ae
plaxidia <plaxidiahove@gmail.com>
2022-03-22 01:56:40 +0300

    added 3 functions ,main ,function f(x) and a function implemating the bisection method

---
+++ bisection_method.cpp
@@ -0,0 +1,57 @@
+//impliment the algorithm
+#include <iostream>
+#include <string>
+#include <ostream>
+#include <cmath>
+using namespace std;
+double f(double x)
+{
+return pow(x,2)*sin(2*x);
+}
+void bisection(double a , double b,double tol)
+
+{
+    int i = 0;
+    if (f(a)*f(b)>0)
+    {
+        cout << "wrong guess"<<endl;
+    }
+    double a=c;
+while 
+ {
+    c=a+(b-a)/2;
+        if(fabs(f(c)<tol||(b-a)/2<tol))
+      {
+         cout<<c<<endl;
+         break;
+         i=i+1;
+        
+         else if (c*a<0)
+         {
+            b=c;
+         }
+         else
+         a=c;
+     }
+    cout << "The value of root is : " << c;
+   
+ }
+
+}
+
+}
+int main()
+{
+while(double a =0.0,b=1.0,c)
+{
+
+double tol ;
+cout<<"the tolarence is = "<<endl;
+cin>>tol;
+double N;
+cout<<" maximaum number of iterations = "<<endl;
+cin>>N; 
+int i = 0;
+bisection(a,b, tol)
+}
+}
